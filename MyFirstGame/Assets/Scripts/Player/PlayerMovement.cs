3c8f22909b51e3048dd96e66b411edc6003dfe2a
melisamessa <melimessa84@gmail.com>
2024-05-09 00:39:06 -0300

    first play commit

---
+++ MyFirstGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float speed = 10f;
+    private float gravity = -9.81f;
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float sphereRadius = 0.3f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    public float jumpHeight = 3f;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, sphereRadius, groundMask);
+        
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;   //si fuera cero puede dar error
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        characterController.Move(move * speed * Time.deltaTime);
+        
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
